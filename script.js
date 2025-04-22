7d7c7a91fa29cf42d7df75315bffef74fcda4096
saksham.g1 <saksham.g@blute.co.in>
2025-04-22 15:33:21 +0530

    Mouse Move and Camera Move

--- script.js
+++ script.js
@@ -1,57 +1,64 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-const canvas= document.querySelector('canvas.webgl')
-
-const scene = new THREE.Scene(); // scene
-scene.add(new THREE.AxesHelper(2))
-
-// Mesh = Object in Three js
-// Mesh = Geometry + Material
-
-const group = new THREE.Group()
-const cube1 = new THREE.Mesh(new THREE.BoxGeometry( 1, 1, 1 ),new THREE.MeshBasicMaterial( {color: 0xFF0000} ))
-const cube2 = new THREE.Mesh(new THREE.BoxGeometry( 1, 1, 1 ),new THREE.MeshBasicMaterial( {color: 0xFFFFFF} ))
-cube2.position.x= -2
-const cube3 = new THREE.Mesh(new THREE.BoxGeometry( 1, 1, 1 ),new THREE.MeshBasicMaterial( {color: 0x00ff00} ))
-cube3.position.x= 2
-group.add(cube1,cube2,cube3);
-scene.add(group)
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 
-
-group.rotation.y=10
-// Camera
-// One type of camera is PerspectiveCamera, two parameters main
-// Field of View and aspect ratio
-
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-const camera= new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
-camera.position.z= 5
-camera.lookAt(group.position)
+const cursor = {
+    X: 0,
+    Y:0 ,
+}
+window.addEventListener('mousemove',(e)=>{
+    cursor.X= e.clientX/sizes.width -0.5;
+    cursor.Y= -(e.clientY/sizes.height -0.5);
+    console.log(cursor.X,cursor.Y)
+})
+// Scene
+const scene = new THREE.Scene()
 
-// Renderer- it renders the scene from the camera prespective, draws it on canvas
-// We are going to use WEBGL renderer
-const renderer= new THREE.WebGLRenderer({
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 2
+
+
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setSize(sizes.width,sizes.height)
+renderer.setSize(sizes.width, sizes.height)
 
-var clock = new THREE.Clock()
-const tick = ()=>{
+// Animate
 
-    var deltaTime = clock.getElapsedTime()
-    console.log(deltaTime)
-    camera.position.x=Math.sin(deltaTime * Math.PI )
-    camera.position.y=Math.cos(deltaTime * Math.PI )
-    camera.lookAt(group.position)
-    renderer.render(scene,camera)
+
+const tick = () =>
+{   
+    camera.position.x=Math.sin(cursor.X*Math.PI*2)*3
+    camera.position.z=Math.cos(cursor.X*Math.PI*2)*3
+    camera.position.y=cursor.Y*5
+    camera.lookAt(mesh.position)
+    // Render   
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
-   
 }
-
 
 tick()
